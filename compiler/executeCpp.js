9c2309c312c99b655779c1a023d2f123b6a8f997
singla-rhydham <singlarhydham2004@gmail.com>
2025-08-03 18:04:36 +0530

    fix issue for deployment

--- compiler/executeCpp.js
+++ compiler/executeCpp.js
@@ -7,18 +7,23 @@ if (!fs.existsSync(outputFolder)) fs.mkdirSync(outputFolder, { recursive: true }
 
 const executeCpp = async (filePath, inputFilePath) => {
     const outputId = path.basename(filePath).split(".")[0];
-    const outputPath = path.join(outputFolder, `${outputId}.exe`);
+    const outputPath = path.join(outputFolder, `${outputId}.out`);
+
 
     return new Promise((resolve, reject) => {
+        // Compile: same for Linux (as long as you use g++)
         exec(`g++ "${filePath}" -o "${outputPath}"`, (err, stdout, stderr) => {
             if (err) return reject({ err, stderr });
 
-            exec(`cmd /c "${outputPath}" < "${inputFilePath}"`, (runErr, runStdout, runStderr) => {
+            // Run the binary: No cmd /c, just use the Linux path!
+            // Use 'sh -c' to support input redirection
+            exec(`sh -c '"${outputPath}" < "${inputFilePath}"'`, (runErr, runStdout, runStderr) => {
                 if (runErr) return reject({ runErr, runStderr });
                 resolve(runStdout);
             });
         });
     });
+
 };
 
 module.exports = executeCpp;
